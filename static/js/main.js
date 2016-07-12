3ba27241bfc0dc91360f844ae7c95b177443dd10
Ivan Tadeu Ferreira Antunes Filho <itadeufa@gmail.com>
2016-07-12 01:44:09 -0400

    Can use the return button on a cellphone, as well as link to the modal windows of the rooms

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,38 @@
 
- var foo = $('.gallery-photo');
+var foo = $('.gallery-photo');
 foo.poptrox({
-						selector:                   'a.image', 
-						usePopupNav: true,
-						baseZIndex:2000,
-					});
+    selector:                   'a.image', 
+    usePopupNav: true,
+    baseZIndex:2000,
+});
+
+$('div.modal').on('show.bs.modal', function() {
+    var modal = this;
+    var hash = modal.id;
+    window.location.hash = hash;
+    window.onhashchange = function() {
+	if (!location.hash){
+	    $(modal).modal('hide');
+	}
+    }
+});
+
+$('div.modal').on('hide.bs.modal', function() {
+    var hash = this.id;
+    history.pushState('', document.title, window.location.pathname);
+});
+
+
+$(document).ready(function() {
+    $('div.modal').each(function(){
+	var modal = this;
+	console.log(this);
+	console.log(this.modal);
+	var hash = '#'+modal.id;
+	var windowhash = window.location.hash;
+	if(windowhash== hash) {
+	    $(this).modal('toggle');
+	}
+    });
+});
+
