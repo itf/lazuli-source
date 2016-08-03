2129774c7177516c0672944073be68c7382185bb
Ivan Tadeu Ferreira Antunes Filho <itadeufa@gmail.com>
2016-08-03 01:10:51 -0400

    Makes use of baseline jpeg again

--- static/img/portfolio/resize.sh
+++ static/img/portfolio/resize.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #mogrify -path ./thumb -resize 75x50 -strip -interlace Plane -gaussian-blur 0.05 -quality 15 -compress Group4 -format jpg *.jpg
  
-mogrify -path ./smaller  -strip -interlace Plane  -quality 75  -format jpg *.jpg
+#mogrify -path ./smaller  -strip -interlace Plane -sampling-factor 4:2:0 -quality 75  -format jpg *.jpg
+
+mogrify -path ./smaller  -strip -interlace none -sampling-factor 4:2:0 -quality 75  -format jpg *.jpg
